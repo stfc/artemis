23875f1b6dfb957d078fb3d79fe9cf4285772894
James Adams <james.adams@stfc.ac.uk>
2009-10-12 17:31:12 +0000

    Changed structure of artemis modules.
    git-svn-id: https://svn.esc.rl.ac.uk/repos/pscsg/artemis/trunk@5191 52211617-7e5b-0410-93e2-a8078075ee8e

---
+++ artemis_node_xml_swift.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+#
+#  Copyright Science and Technology Facilities Council, 2009.
+#  
+#  This file is part of ARTEMIS.
+#  
+#  ARTEMIS is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  ARTEMIS is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with ARTEMIS. If not, see <http://www.gnu.org/licenses/>.
+#
+#  $Revision$
+#  $Date$
+#  $LastChangedBy$
+#
+
+from artemis_node_base import node
+
+##
+# 1-Wire Network attached monitoring base, using XML interface instead of SNMP
+#
+class node_swiftCM1_xml(node):
+  def fetch(self):
+    from xml.dom import minidom
+
+    #Fetch data
+    data = urllib.urlopen('http://' + self.ip + '/data.xml')
+    data = minidom.parse(data)
+
+    return data
+
+    #temperature probes
+    if (i != None):
+      ids = i
+    else:
+      ids = []
+
+    v = getMIB(self.ip, ".1.3.6.1.4.1.17373.2.4.1.5")
+    if (v != None):
+      values = v
+    else:
+      values = []
+
+    #airflow sensors
+    i = getMIB(self.ip, ".1.3.6.1.4.1.17373.2.5.1.2")
+    if (i != None):
+      ids += i
+
+    v = getMIB(self.ip, ".1.3.6.1.4.1.17373.2.5.1.5")
+    if (v != None):
+      values += v
+
+    units = [FAMILY_1WIRE[i[-2:]][1] for i in ids]
+
+    #This may look confusing, it's just splitting the ID up into parts
+    ids   = [FAMILY_1WIRE[i[-2:]][0] + "-1WIRE-" + i[2:-2] + i[:2] for i in ids]
+    
+    values = [int(v) for v in values]
+
+    return zip(ids, values, units)
