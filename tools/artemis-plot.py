b0f55216c60a75d26f34d5af601aaf90b18d7c47
James Adams <james.adams@stfc.ac.uk>
2011-04-12 13:59:13 +0000

    Added boilerplate and enable keywords. Pulled in some ancient change I have forgotten about.
    git-svn-id: https://svn.esc.rl.ac.uk/repos/pscsg/artemis/trunk@8555 52211617-7e5b-0410-93e2-a8078075ee8e

--- tools/artemis-plot.py
+++ tools/artemis-plot.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+#
+#  Copyright Science and Technology Facilities Council, 2009.
+#  
+#  This file is part of ARTEMIS.
+#  
+#  ARTEMIS is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  ARTEMIS is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with ARTEMIS. If not, see <http://www.gnu.org/licenses/>.
+#
+#  $Revision$
+#  $Date$
+#  $LastChangedBy$
+#
 
 import json, urllib2, datetime
 
@@ -57,8 +79,12 @@ def plot(x, y, z, title, filename):
   plt.colorbar()
   
   f = "hm/hm_%s.png" % filename
-  plt.savefig(f)
-  print("Wrote " + f)
+  if MODE == 1:
+    plt.savefig(f)
+    print("Wrote " + f)
+  else:
+    plt.show()
+
   plt.clf()
 
 
