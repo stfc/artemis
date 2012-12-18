a13a37ad2afe3d937368ed5e83847f994a6569da
James Adams <james.adams@stfc.ac.uk>
2012-12-18 14:31:44 +0000

    Correct filename of config module.

--- artemis_config.py
+++ artemis_config.py
@@ -1,40 +1,32 @@
-#!/usr/bin/python
+#!/usr/bin/python26
 #
 #  Copyright Science and Technology Facilities Council, 2009.
-#  
+#
 #  This file is part of ARTEMIS.
-#  
+#
 #  ARTEMIS is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  ARTEMIS is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with ARTEMIS. If not, see <http://www.gnu.org/licenses/>.
 #
-#  $Revision$
-#  $Date$
-#  $LastChangedBy$
-#
+
+import ConfigParser
 
 #Depends on node classes from artemis_core
-from artemis_core import *
+from nodetypes.snmp_env_jacarta import *
+from nodetypes.snmp_env_swift import *
+from nodetypes.snmp_pdu_apc import *
+from nodetypes.xml_env_swift import *
 
-#Round Robin Database configuration
-rrd_dir = "rrds/"
-rrd_ext  = ".rrd"
+from artemis_store import session, Node, Probe
 
-#Sensor base node configuration
-#  CLASS(IP-ADDRESS, COMMUNITY (defaults to 'public'))
-base_nodes = [
-]
-
-#Location information for sensors
-#  IDENTIFIER : (FRIENDLY-NAME, X-POS, Y-POS, WIDTH, HEIGHT)
-sensors = {
-}
+config = ConfigParser.ConfigParser()
+config.read(['artemis.conf'])
